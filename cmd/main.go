465cb1876445cb38ab210cd68b8f1348c1528bff
Turtel216 <papadimitrios216@gmail.com>
2024-11-22 02:33:15 +0200

    Added Linux namespace creation

--- cmd/main.go
+++ cmd/main.go
@@ -3,24 +3,53 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
+	"syscall"
 )
 
 func main() {
 	switch os.Args[1] {
 	case "run":
 		run()
-
+	case "child":
+		child()
 	default:
 		panic("Invalid command input")
 	}
+}
+
+func run() {
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+
+	// Remap IO
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	// Add Linux Namespace
+	cmd.SysProcAttr = &syscall.SysProcAttr{
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID,
+	}
+
+	cmd.Run()
+}
+
+func child() {
+	fmt.Printf("Sticky running %v with pid: %d\n", os.Args[2:], os.Getpid())
+
+	syscall.Sethostname([]byte("container"))
+
+	cmd := exec.Command(os.Args[2], os.Args[3:]...)
+	// Remap IO
+	cmd.Stdin = os.Stdin
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	cmd.Run()
 }
 
 func must(err error) {
 	if err != nil {
 		panic(err)
 	}
-}
-
-func run() {
-	fmt.Printf("Sticky running %v\n", os.Args[2:])
 }
